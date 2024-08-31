a7c266755841ed17ad30933344a69d1422930869
xCyn <155592561+xCynDevelopment@users.noreply.github.com>
2024-08-31 16:33:29 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,24 +1,3 @@
--- local QBCore = exports['qb-core']:GetCoreObject()
+Config = {}
 
--- RegisterNetEvent('xcyn:target:prisonclothes', function()
---     TriggerEvent("tgiann-clothing:openOutfitMenu")
--- end)
-
-
--- exports['qb-target']:AddBoxZone("PrisonClothes", vector3(1740.84, 2497.41, 46.17), 1, 1, {
--- 	name = "PrisonClothes",
--- 	heading = 306.96,
--- 	debugPoly = false,
--- 	minZ = 44.80,
--- 	maxZ = 47.80,
--- }, {
--- 	options = {
--- 		{
---             type = "client",
---             event = "xcyn:target:prisonclothes",
--- 			icon = "fas fa-sign-in-alt",
--- 			label = "Change Clothes",
--- 		},
--- 	},
--- 	distance = 2.5
--- })
+Config.Inventory = "tgiann-inventory"  ---- For HasItem Export
